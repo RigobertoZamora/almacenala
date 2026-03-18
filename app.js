f6a7801ff0c4dfbd56a28306ad6f7b9b1a1bafa5
RigoZa <rigoprogrammer10@gmail.com>
2026-03-18 16:30:43 -0600

    Feature: Add backend cloud api

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api/';
+const API_URL = 'https://api-almacen-backend.onrender.com';
 let catalogoActual = ""; // Guardará 'conceptos', 'destinos', etc.
 
 // ==========================================
